421043ae18c44ce2501a5bc084e71afd07a04fe2
hangaoke1 <188869009@qq.com>
2018-04-15 15:18:43 +0800

    更新

--- public/tumbler-single/js/config.js
+++ public/tumbler-single/js/config.js
@@ -4,11 +4,11 @@
     "DEAFULT_RATE": 0.5, // 无人状态下转盘速度0 - 1
     "MAX_RATE": 1.3, // 转盘速度上限
     "MIN_RATE": 0.2, // 转盘速度下限
-    "NUM_EACH_SHAPE": 10, // 每种形状的个数
+    "NUM_EACH_SHAPE": 10, // 每种形状的个数 相当于初始状态下 20个图形
     "COLOR": ["#f0dfb9", "#e8b997", "#3f4e63", "#679fab", "#7badbf", "#b0c6d1"], //颜色参数
     "DRUM_ANGULAR_VELOCITY": Math.PI / 3, // 转盘速度
-    "MIN_AUDIO_PLAYBACK_RATE": 0.3,
-    "MAX_AUDIO_PLAYBACK_RATE": 0.5,
+    "MIN_AUDIO_PLAYBACK_RATE": 0.3, // 声音下限 原: 0.15
+    "MAX_AUDIO_PLAYBACK_RATE": 0.5, // 声音上限 原: 0.75
     "WORLD_WIDTH": 10,
     "WORLD_HALF_WIDTH": 5,
     "NUM_DRUM_SECTIONS": 32,
